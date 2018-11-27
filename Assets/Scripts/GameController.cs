67e899ca7ebc570aa6a988f20fb3d963110a680f
316Douji <511599737@qq.com>
2018-11-27 16:42:12 +0800

    UI added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,19 +12,17 @@ public class GameController : MonoBehaviour {
     public static bool isBedMainLightEnable = false;
     public static bool isBedSofaLightEnable = false;
     public static bool isBedDecorateLightEnable = false;
-
-
+    public static List<GameObject> lights =new List<GameObject>();
 
     public GameObject Ground;
     public GameObject Table1, Table2;
     public GameObject Bed;
     public GameObject Wall;
     public GameObject Sofa;
-    public GameObject Ceiling;
+    public GameObject Ceiling, Sceiling;
     // Use this for initialization
     void Start () {
-		
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -33,14 +31,16 @@ public class GameController : MonoBehaviour {
 
     public void TempLayout() {
         ClearAllLight();
-        GenerateSpotLight(GetTableLightPos(Table1, Bed));
-        GenerateSpotLight(GetTableLightPos(Table2, Bed));
-        GeneratePointLight(GetMainLightPos());
-        GeneratePointLight(GetLampPos(Sofa));
+        GeneLight(2, GetTableLightPos(Table1, Bed));
+        GeneLight(2, GetTableLightPos(Table2, Bed));
+        GeneLight(1, GetMainLightPos(1));
+        GeneLight(1, GetSofaLightPos());
     }
 
 
     public void ClearAllLight() {
+        if(lights!=null&&lights.Count>0)
+        lights.Clear();
         GameObject[] allLight = GameObject.FindGameObjectsWithTag("LightGe");
         if (allLight.Length > 0) {
             foreach (GameObject go in allLight)
@@ -84,14 +84,14 @@ public class GameController : MonoBehaviour {
     }
 
 
-    //得到模型中间最上方位置
+    //通用计算：得到模型中间最上方位置
     public Vector3 GetLampPos(GameObject GO)
     {
         var aabb = CalcBounds(GO);
         return new Vector3(aabb.center.x, aabb.center.y + aabb.extents.y, aabb.center.z);
     }
 
-    //以床头为高，桌子中央为位置，初始化床头灯光
+    //床头灯位置计算： 以床头为高，桌子中央为位置，初始化床头灯光
     public Vector3 GetTableLightPos(GameObject Fun, GameObject Bed)
     {
         var aabbTable = CalcBounds(Fun);
@@ -100,36 +100,61 @@ public class GameController : MonoBehaviour {
 
     }
 
-    //以房间中心为xz，床高加2m为高
-    public Vector3 GetMainLightPos()
+    //顶灯位置计算： 以房间中心为xz，床高加2m为高
+    public Vector3 GetMainLightPos(int FunNum)
     {
-        var aabbCeiling = CalcBounds(Ceiling);
-        var aabbBed = CalcBounds(Bed);
-        return new Vector3(aabbCeiling.center.x, aabbBed.center.y + 2f, aabbCeiling.center.z);
-
+        if (FunNum == 0)
+        {
+            var aabbCeiling = CalcBounds(Ceiling);
+            var aabbBed = CalcBounds(Bed);
+            return new Vector3(aabbCeiling.center.x, aabbBed.center.y + 2f, aabbCeiling.center.z);
+        }
+        else {
+            var aabbSceiling = CalcBounds(Sceiling);
+            var aabbBed = CalcBounds(Bed);
+            return new Vector3(aabbSceiling.center.x, aabbBed.center.y + 2f, aabbSceiling.center.z);
+        }
     }
 
-    //以某扶手为中心，沙发高度加1m为高
+    //沙发灯位置计算： 以某扶手为中心，沙发高度加1m为高
     public Vector3 GetSofaLightPos()
     {
-        var aabbCeiling = CalcBounds(Ceiling);
-        var aabbBed = CalcBounds(Bed);
-        return new Vector3(aabbCeiling.center.x, aabbBed.center.y + 2f, aabbCeiling.center.z);
+        var aabbSofa = CalcBounds(Sofa);
+        return new Vector3(aabbSofa.center.x-aabbSofa.extents.x, aabbSofa.center.y + 2*aabbSofa.extents
+            .y, aabbSofa.center.z - aabbSofa.extents.z);
 
     }
 
-    public void GenerateSpotLight(Vector3 vec)
-    {
-        GameObject Spotgroup = Resources.Load("SpotGroup") as GameObject;
-        GameObject sg = Instantiate(Spotgroup) as GameObject;
-        sg.transform.position = vec;
+
+    /// <summary>
+    /// 1:PointLight
+    /// 2:SpotLightGroup
+    /// 3.SpotLight
+    /// </summary>
+    /// <param name="LightType"></param>
+    public void GeneLight(int LightType, Vector3 pos) {
+        switch (LightType) {
+            case 1:
+                GameObject Pointgroup = Resources.Load("PointGroup") as GameObject;
+                GameObject pg = Instantiate(Pointgroup) as GameObject;
+                MainLightCount += 1;
+                pg.name = "点光源" + MainLightCount;
+                pg.transform.position = pos;
+                lights.Add(pg);
+                break;
+            case 2:
+                GameObject Spotgroup = Resources.Load("SpotGroup") as GameObject;
+                GameObject sg = Instantiate(Spotgroup) as GameObject;
+                TableLightCount += 1;
+                sg.name = "聚光灯组" + TableLightCount;
+                sg.transform.position = pos;
+                lights.Add(sg);
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
     }
 
-    public void GeneratePointLight(Vector3 vec)
-    {
-        GameObject Pointgroup = Resources.Load("PointGroup") as GameObject;
-        GameObject pg = Instantiate(Pointgroup) as GameObject;
-        pg.transform.position = vec;
-
-    }
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -4,47 +4,73 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
-    public Toggle BedTableToggle;
-    public Toggle MainToggle;
-    public Toggle SofaToggle;
-    public Toggle DecorateToggle;
+    public GameObject ControllerList;
+    //public Toggle BedTableToggle;
+    //public Toggle MainToggle;
+    //public Toggle SofaToggle;
+    //public Toggle DecorateToggle;
 
-	// Use this for initialization
-	void Start () {
+    int PanelGenerated = 0;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        
 
     }
 
-    public void lightControl(int LightNum) {
-        switch (LightNum) {
-            case 0:
-                break;
-            case 1:
-                GameController.isBedTableLightEnable = BedTableToggle.isOn;
-                print("isBedTableLightEnable"+GameController.isBedTableLightEnable);
-                break;
-            case 2:
-                GameController.isBedMainLightEnable = MainToggle.isOn;
-                print("isBedMainLightEnable" + GameController.isBedMainLightEnable);
+    public void InitList() {
+        //GameController.TableLightCount
+        foreach (GameObject lit in GameController.lights) {
+            if (lit.gameObject.tag == "PointGe")
+            {
+                GeneratePanel(1,lit); 
+                PanelGenerated += 1;
+            }
+            else if (lit.gameObject.tag == "SpotGroupGe")
+            {
+                GeneratePanel(2, lit);
+                PanelGenerated += 1;
+            }
+            else {
 
-                break;
-            case 3:
-                GameController.isBedSofaLightEnable = SofaToggle.isOn;
-                print("isBedSofaLightEnable" + GameController.isBedSofaLightEnable);
-
-                break;
-            case 4:
-                GameController.isBedDecorateLightEnable = DecorateToggle.isOn;
-                print("isBedDecorateLightEnable" + GameController.isBedDecorateLightEnable);
-                break;
-            default:
-                break;
+            }
         }
+
+    }
+
+
+    /// <summary>
+    /// 根据灯光类型生成相应的控制面板
+    /// </summary>
+    /// <param name="panelType">1.point 2.spotgroup 3.spot</param>
+    /// <param name="PosY"></param>
+    public void GeneratePanel(int panelType,GameObject Lit) {
+        GameObject ConPanel =Instantiate(Resources.Load("LightControlPanel")) as GameObject;
+
+        ConPanel.transform.SetParent(ControllerList.transform);
+        ConPanel.transform.localPosition = Vector3.zero;
+        ConPanel.transform.localScale = Vector3.one;
+        ConPanel.transform.localPosition += new Vector3(0, -52f * PanelGenerated, 0);
+
+        ConPanel.GetComponentInChildren<Text>().text = Lit.name;
+
+        Toggle m_Toggle = ConPanel.GetComponentInChildren<Toggle>();
+        m_Toggle.onValueChanged.AddListener(delegate {
+        ToggleValueChanged(m_Toggle, Lit);
+              });
+    }
+
+    void ToggleValueChanged(Toggle change, GameObject Lit)
+    {
+        Lit.SetActive(change.isOn);
+    }
+
+
+    public void ClearAllController() {
+        
     }
 }
